7f7f27ba7f7496bbae6e15dd6ac11e14031e4f20
dv0c <tasosmeidanis12@gmail.com>
2024-05-19 21:50:07 +0300

    new file:   .env 	new file:   prisma/schema.prisma 	new file:   public/assets/images/login_splash.jpg 	modified:   src/app/(routes)/login/page.tsx

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -1,22 +1,35 @@
 import Image from "next/image"
 
 import StateHandler from "./_comps/StateHandler"
+import Link from "next/link"
+import { ArrowLeft } from "lucide-react"
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+
+import LoginImage from '../../../../public/assets/images/login_splash.jpg'
 
 export default function page() {
     return (
-        <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[100vh]">
-            <div className="flex items-center justify-center py-12">
-                <StateHandler />
+        <>
+            <Link href={'/'} className={cn('absolute top-5 left-3', buttonVariants({ variant: "ghost" }))}>
+                <ArrowLeft size={20} className="mr-2" />
+                Go back
+            </Link>
+            <div className="w-full lg:grid grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[100vh]">
+                <div className="flex items-center justify-center py-12">
+                    <StateHandler />
+                </div>
+                <div className="hidden bg-muted lg:block">
+                    <Image
+                        src={LoginImage}
+                        alt="Image"
+                        width="1920"
+                        height="1080"
+                        placeholder="blur"
+                        className="h-full w-full object-cover dark:brightness-[.5]"
+                    />
+                </div>
             </div>
-            <div className="hidden bg-muted lg:block">
-                <Image
-                    src="/placeholder.svg"
-                    alt="Image"
-                    width="1920"
-                    height="1080"
-                    className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                />
-            </div>
-        </div>
+        </>
     )
 }
