0b152a726b049e7b92caa12a31a91a6d0bb7427c
dv0c <tasosmeidanis12@gmail.com>
2024-05-20 10:24:36 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/(routes)/login/_comps/LoginForm.tsx 	modified:   src/app/(routes)/login/_comps/RegisterForm.tsx 	new file:   src/components/ui/form.tsx

--- src/app/(routes)/login/_comps/LoginForm.tsx
+++ src/app/(routes)/login/_comps/LoginForm.tsx
@@ -1,9 +1,57 @@
+'use client'
 import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Loader2 } from "lucide-react"
+import { signIn } from "next-auth/react"
 import Link from "next/link"
+import { useSearchParams } from "next/navigation"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+
+const formSchema = z.object({
+    email: z.string().min(2, { message: "Email must be at least 2 characters" }).email({ message: "Email must be valid" }),
+    password: z.string().min(6, {
+        message: "Email must be at least 6 characters"
+    }).max(50, {
+        message: "Password must be less than 50 characters"
+    })
+})
 
 const LoginForm = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const error = useSearchParams().get("error")
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        },
+    })
+
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        try {
+            setIsLoading(true);
+            await signIn("credentials", {
+                email: values.email,
+                password: values.password,
+                callbackUrl: "/dashboard",
+            }).finally(() => setIsLoading(false))
+        } catch (error) {
+            setIsLoading(false);
+            console.log(error);
+        }
+    }
+
+    async function handleGoogle() {
+        setIsLoading(true)
+        await signIn("google", { callbackUrl: process.env.REDIRECT_AFTER_LOGIN || '/' }).finally(() => setIsLoading(false));
+    }
     return <div className="mx-auto grid w-[350px] gap-6 p-5 md:p-0">
         <div className="grid gap-2 text-center">
             <h1 className="text-3xl font-bold">Welcome Back</h1>
@@ -11,35 +59,49 @@ const LoginForm = () => {
                 Enter your email below to login to your account
             </p>
         </div>
-        <div className="grid gap-4">
-            <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+                <FormField disabled={isLoading}
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="email@example.com" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
-            </div>
-            <div className="grid gap-2">
-                <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <Link
-                        href="/forgot-password"
-                        className="ml-auto inline-block text-sm underline"
-                    >
-                        Forgot your password?
-                    </Link>
-                </div>
-                <Input id="password" type="password" required />
-            </div>
-            <Button type="submit" className="w-full">
-                Sign in
-            </Button>
-            <Button variant="outline" className="w-full">
-                Login with Google
-            </Button>
-        </div>
+                <FormField
+                    disabled={isLoading}
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input placeholder="*****" type="password" {...field} />
+                            </FormControl>
+                            <Link
+                                href="/forgot-password"
+                                className="ml-auto inline-block text-sm underline"
+                            >
+                                Forgot your password?
+                            </Link>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button disabled={isLoading} type="submit" className="w-full">
+                    {isLoading ? <Loader2 size={20} className="animate-spin" /> : "Sign in"}
+                </Button>
+                <Button onClick={handleGoogle} variant="outline" className="w-full">
+                    Continue with Google
+                </Button>
+            </form>
+        </Form>
         <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{" "}
             <Link href="/login?state=register" className="underline">
--- src/app/(routes)/login/_comps/RegisterForm.tsx
+++ src/app/(routes)/login/_comps/RegisterForm.tsx
@@ -1,46 +1,125 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { signIn } from "next-auth/react"
 import Link from "next/link"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import axios from 'axios'
+import { useRouter } from "next/navigation"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Loader2 } from "lucide-react"
+
+const formSchema = z.object({
+    email: z.string().min(2, { message: "Email must be at least 2 characters" }).email({ message: "Email must be valid" }),
+    password: z.string().min(6, {
+        message: "Email must be at least 6 characters"
+    }).max(50, {
+        message: "Password must be less than 50 characters"
+    }),
+    username: z.string().min(2, { message: "Username must be at least 2 characters" }).max(50, { message: "Username must be less than 16 characters" })
+})
 
 const RegisterForm = () => {
-    return <div className="mx-auto grid w-[350px] gap-6 p-5 md:p-0">
-        <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Create an account</h1>
-            <p className="text-sm text-muted-foreground">
-                Enter your email below to create an account
-            </p>
-        </div>
-        <div className="grid gap-4">
-            <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                />
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    // const error = useSearchParams().get("error")
+    const router = useRouter()
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+            username: ""
+        },
+    })
+
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        try {
+            setIsLoading(true);
+            await axios.post('/api/auth/register', values).finally(() => setIsLoading(false)).then(() => {
+                router.push(process.env.REDIRECT_AFTER_LOGIN || '/')
+            })
+        } catch (error: any) {
+            setIsLoading(false);
+            setError(error.response.data)
+        }
+    }
+
+    async function handleGoogle() {
+        setIsLoading(true)
+        await signIn("google", { callbackUrl: process.env.REDIRECT_AFTER_LOGIN || '/' }).finally(() => setIsLoading(false));
+    }
+    return (
+        <div className="mx-auto grid w-[350px] gap-6 p-5 md:p-0">
+            <div className="grid gap-2 text-center">
+                <h1 className="text-3xl font-bold">Create an account</h1>
+                <p className="text-sm text-muted-foreground">
+                    Enter your email below to create an account
+                </p>
             </div>
-            <div className="grid gap-2">
-                <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                </div>
-                <Input id="password" type="password" required />
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+                    <FormField disabled={isLoading}
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="dv0c" type="text" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField disabled={isLoading}
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="email@example.com" type="email" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        disabled={isLoading}
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="*****" type="password" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button disabled={isLoading} type="submit" className="w-full">
+                        {isLoading ? <Loader2 size={20} className="animate-spin" /> : "Create Account"}
+                    </Button>
+                    <Button onClick={handleGoogle} disabled={isLoading} type="button" variant="outline" className="w-full">
+                        Continue with Google
+                    </Button>
+                </form>
+            </Form>
+            <div className="mt-4 text-center text-sm">
+                Already have an account?{" "}
+                <Link href="/login?state=login" className="underline">
+                    Sign in
+                </Link>
             </div>
-            <Button type="submit" className="w-full">
-                Create Account
-            </Button>
-            <Button variant="outline" className="w-full">
-                Continue with Google
-            </Button>
         </div>
-        <div className="mt-4 text-center text-sm">
-            Already have an account?{" "}
-            <Link href="/login?state=login"className="underline">
-                Sign in
-            </Link>
-        </div>
-    </div>
+    )
 }
 
 export default RegisterForm
